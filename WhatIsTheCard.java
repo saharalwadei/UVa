29ee377eb0f1e1a43349a91fedfb77658ded77f5
saharalwadei <saharalwadei@gmail.com>
2018-09-23 14:03:28 +0300

    Rename it to Main
    The class name must be Main and the package name should be removed if NetBeans or similar IDEs used.

--- WhatIsTheCard.java
+++ WhatIsTheCard.java
@@ -1,8 +1,7 @@
-package whatisthecard;
 
 import java.util.Scanner;
 // UVa 10646
-public class WhatIsTheCard {
+public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
